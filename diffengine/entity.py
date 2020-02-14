7b04acf18cf96034d05d8a5cbbcc974683775ece
Damien <damien.bonneau2@gmail.com>
2020-02-14 22:42:37 +0100

    basic game logic works

--- diffengine/entity.py
+++ diffengine/entity.py
@@ -6,7 +6,8 @@ import sys
 def _print(*a, **kw):
     print(*a, **kw)
     sys.stdout.flush()
-    
+
+import logic as lg    
 
 class Entity(object):
     '''
@@ -154,21 +155,81 @@ class Container(object):
         for o in self.entities:
             _x, _y = o.get_position()
             surface.blit(o.get_surface(), (_x-x, _y-y))
+       
+class RegisterGUI(Entity):
+    def __init__(self,  position, size=16, h=20, w=20):
+        self.COLOR_BCKGND = pg.Color('black')
+        self.COLOR_SPACER = pg.Color('gray21')
+        self.COLOR_BORDER = pg.Color('gray66')
+        self.COLOR_DIGIT = pg.Color('gray66')
+        self.surface = pg.Surface((w * size, h))
+        self.bbrect = pg.Rect(0, 0, w * size, h)
+        self.FONT = pg.font.Font(pg.font.match_font("couriernew"), 16, bold=True)
+        self.w = w
+        self.h = h
+        self.size = size
+        self.digits_to_surf = {i :self.FONT.render(f"{i}", True, self.COLOR_DIGIT) for i in [0, 1]}
+        self.init_surface()
+        self.digits = [0] * 16
+        super(RegisterGUI, self).__init__(surface=self.surface, position=position)
+
+    def init_surface(self):
+        self.surface.fill(self.COLOR_BCKGND)
+        w = self.w
+        h = self.h
+        for i in range(self.size):
+            _rect = pg.Rect(i * w, 0, w, h)
+            pg.draw.rect(self.surface, self.COLOR_SPACER, _rect, 1)
             
-class InputBox(Entity):
-    def __init__(self, position, w, h, text='', plan=1):
-        self.COLOR_INACTIVE = pg.Color('darkgreen')
-        self.COLOR_ACTIVE = pg.Color('green')
-        self.COLOR_BORDER_INACTIVE = pg.Color('gray21')
-        self.COLOR_BORDER_ACTIVE = pg.Color('gray66')
+        pg.draw.rect(self.surface, self.COLOR_BORDER, self.bbrect, 3)
+    
+    def set_digit(self, i, digit):
+        self.digits[i] = digit
+        self.update_surface()
         
-        self.color_border = self.COLOR_BORDER_INACTIVE
-        self.color = self.COLOR_INACTIVE
-        self.FONT = pg.font.Font(None, 24)
-                rect = pg.Rect(0, 0, w, h)
+    def set_digits(self, list_digits):
+        self.digits = list_digits
+        self.update_surface()
+        
+    def update_surface(self):
+        self.init_surface()
+        
+        for i,d in enumerate(self.digits):
+            self.surface.blit(self.digits_to_surf[d], (self.w*i+ 2, 1))
+        
+        
+    # def set_digit(self, i, digit):
+        # self.surface.blit(self.digits_to_surf[digit], (w*i, 0))
+        
+    
+    def activate(self):
+        pass
+    
+    def deactivate(self):
+        pass
+        
+COLOR_BORDER_EXEC = pg.Color('cyan')
+COLOR_BORDER_ERROR = pg.Color('red')
+COLOR_INACTIVE = pg.Color('darkgreen')
+COLOR_ACTIVE = pg.Color('green')
+COLOR_BORDER_INACTIVE = pg.Color('gray21')
+COLOR_BORDER_ACTIVE = pg.Color('gray66')
+        
+class InputBox(Entity):
+    def __init__(self, position, w, h, text='', plan=1, max_length_text=12, line_index=0):
+        
+        self.color_border = COLOR_BORDER_INACTIVE
+        self.color = COLOR_INACTIVE
+        self.FONT = pg.font.Font(pg.font.match_font("consolas"), 12)
+        self.max_length_text = max_length_text
+        self.line_index = line_index        rect = pg.Rect(0, 0, w, h)
         self.bbrect = pg.Rect(0, 0, w, h)
         self.txt_surface = self.FONT.render(text, True, self.color)
         surface = pg.Surface((w, h))
+        
+        # Callbacks
+        self.set_text_callback = lambda _inputbox : None
+        self.activate_callback = lambda _inputbox : None
         
         super(InputBox, self).__init__(surface = surface,
                                         rect = rect,
@@ -178,19 +239,17 @@ class InputBox(Entity):
         self.text = text
         self.active = False
         self.update_surface()
-        self.activate_callback = lambda _inputbox : None
 
     def activate(self):
         super(InputBox, self).activate()
-        self.color = self.COLOR_ACTIVE 
-        self.color_border = self.COLOR_BORDER_ACTIVE 
+        self.color = COLOR_ACTIVE 
+        self.color_border = COLOR_BORDER_ACTIVE 
         self.update_surface()
-        _print('activate', self.position, self.next_entity.position)
         
     def deactivate(self):
         super(InputBox, self).deactivate()
-        self.color = self.COLOR_INACTIVE
-        self.color_border = self.COLOR_BORDER_INACTIVE
+        self.color = COLOR_INACTIVE
+        self.color_border = COLOR_BORDER_INACTIVE
         self.update_surface()
 
     def handle_event(self, event):       
@@ -199,24 +258,32 @@ class InputBox(Entity):
                 if event.key == pg.K_RETURN or event.key == pg.K_TAB:
                     if pg.key.get_pressed()[pg.K_LSHIFT] or pg.key.get_pressed()[pg.K_RSHIFT]:
                         if self.prev_entity is not None:
-                            self.activate_callback(self.prev_entity)
                             self.deactivate()
+                            self.activate_callback(self.prev_entity)
                     
                     elif self.next_entity is not None:
-                        self.activate_callback(self.next_entity)
                         self.deactivate()
+                        self.activate_callback(self.next_entity)
+                        
                     
                 elif event.key == pg.K_BACKSPACE:
                     self.text = self.text[:-1]
                 else:
-                    self.text += event.unicode
-                
+                    if len(self.text) < self.max_length_text:
+                        self.text += event.unicode
+                        
                 self.text = self.text.upper()
+                self.set_text_callback(self)  
                 self.update_surface()
+    
+    def set_text(self, text):
+        self.text = text
+        self.set_text_callback(self)
+        self.update_surface()
 
     def update_surface(self):
         surface = pg.Surface((self.rect.width, self.rect.height))
-        self.txt_surface = self.FONT.render(self.text, True, self.color)
+        self.txt_surface = self.FONT.render(self.text, True, self.color)  
         surface.blit(self.txt_surface, (5, 5))
         pg.draw.rect(surface, self.color_border, self.bbrect, 1)
         self.set_surface(surface)
@@ -224,134 +291,68 @@ class InputBox(Entity):
     def update(self):
         pass
         
-class ColumnInputBoxes(Container):
-    def __init__(self, position=(0, 0), n_lines=16, w=100, h=20, plan = 1,
-        activate_callback=lambda widget: None):
+class ProcessorGUI(Container):
+    def __init__(
+        self, 
+        processor, 
+        position=(50, 100), 
+        w=120, 
+        h=20, 
+        plan = 1,
+        activate_callback=lambda widget: None
+    ):
         x, y = position
-        entities = [InputBox((x, y + i * h), w, h) for i in range(n_lines)]
-        super(ColumnInputBoxes, self).__init__(entities=entities, position=position, plan=plan)
+        self.processor = processor
+        n_lines = processor.program_stack_size
+        prog_lines = [InputBox((x, y + i * h), w, h, line_index=i) for i in range(n_lines)]
+        
+        for _inputbox in prog_lines:
+            def set_text_callback(inputbox):
+                text = inputbox.text
+                i = inputbox.line_index
+                line_status = self.processor.check_instruction_syntax(text)
+                _print(text, i, line_status)
+                self.processor.set_prog_line(i, text)
+                if line_status != lg.VALID_SYNTAX:
+                    inputbox.color_border = COLOR_BORDER_ERROR
+                    
+            _inputbox.set_text_callback = set_text_callback
+            
+            
+        self.lines = prog_lines
+        self.current_line = self.lines[self.processor.stack_pointer]
+        self.set_program(self.processor.program)
+        self.register_gui = RegisterGUI(size = processor.register_size, position=(x, y - 20))
+        self.register_gui.set_digits(self.processor.register)
+        
+        entities = prog_lines + [self.register_gui]
+        super(ProcessorGUI, self).__init__(entities=entities, position=position, plan=plan)
         
         for e in self.entities:
             e.activate_callback = activate_callback
-            _print(e.position)
             
-        for e1, e2 in zip(self.entities, [self.entities[-1]] + self.entities[:-1]):
-            print (e1.position, e2.position)
+        for e1, e2 in zip(self.lines, [self.lines[-1]] + self.lines[:-1]):
             e1.prev_entity = e2
             e2.next_entity = e1
-
-
-class ImageEntity(Entity):
-    name = None
-    extension = 'png'
-    
-    def __init__(self, position = (0,0), plan = 1, color_key = -1, rect=None):
-        surface = load_image(self.name, self.extension, color_key)
-        super(ImageEntity, self).__init__(surface = surface,
-                                        rect = rect,
-                                        position = position, plan = plan
-                                        )
-        
-    def collides(self, position):
-        '''
-        Select on any visible part 
-        '''
-        if super(ImageEntity, self).collides(position):
-            xc, yc = position
-            x0, y0 = self.position
-            surface = self.get_surface()
-            pix = surface.get_at((int(xc - x0), int(yc-y0)))   
             
-            return pix != surface.get_colorkey()
+    def update(self):
+        pass
             
-        return False
-
-class AnimatedEntity(Entity):
-    name = None    
-    
-    def __init__(self, position = (0,0), 
-            plan = 1,color_key = -1, rect=None,
-            animation_boundary_condition = 'toric',
-            nb_ticks_per_image = 5, surface = None, width = 40):
-        '''
-        animation_boundary_condition: `toric` or `mirror`    
-        nb_ticks_per_image: number of ticks to wait before changing the image
-        '''                
-        
-        
-        self.surface_index = 0
-        self.tick_counter = 0        
-        
-        
-        if surface == None:
-            surface = self.load_animation(width = width)
-        
-        rect = rect if rect!=None else surface.get_rect()
-        rect = pg.Rect(rect.left, rect.top, width, rect.height)
-        
-        
-        super(AnimatedEntity, self).__init__(surface = surface,
-                                            rect = rect,
-                                            position = position, 
-                                            plan = plan
-                                        )
-        self.animation_boundary_condition = animation_boundary_condition
-        self.animation_direction = 1
-        self.nb_ticks_per_image = nb_ticks_per_image
-    
-    
-    def get_surface(self):
-        return self.surface.subsurface(self.animation_rect)    
-    
-    def set_next_surface(self):
-        if self.animation_boundary_condition == 'toric':
-            if self.animation_direction == 1:                
-                self.surface_index += 1
-                dw = self.width 
-                
-                if self.surface_index == self.nb_surfaces:
-                    self.surface_index = 0
-                    dw = -self.width*(self.nb_surfaces-1)
-                    
-            elif self.animation_direction == -1:
-                self.surface_index -= 1
-                dw = -self.width 
-                
-                if self.surface_index < 0:
-                    self.surface_index = self.nb_surfaces - 1
-                    dw = self.width*(self.nb_surfaces-1)
-                
-        elif self.animation_boundary_condition == 'mirror':            
-            if self.animation_direction == 1:
-                self.surface_index += 1
-                if self.surface_index == self.nb_surfaces:
-                    self.surface_index -= 1
-                    self.animation_direction = -1
-            else:
-                self.surface_index -= 1 
-                if self.surface_index <0:
-                    self.surface_index += 1
-                    self.animation_direction = 1
-            dw = self.width*self.animation_direction
-        self.animation_rect.move_ip((dw,0))
-    
-    def load_animation(self, name = None, extension = 'bmp', width = 40, 
-        color_key = -1):
-        if name == None:
-            name = self.name
-            
-        surface = load_image(self.name, extension, color_key)
-        total_width = surface.get_width()
-        height = surface.get_height()
-        self.nb_surfaces = int(total_width / width)
-        
-        self.animation_rect = pg.Rect(0,0,width,height)
-        self.width = width
-        return surface 
-                  
     def tick(self):
-        self.tick_counter += 1
-        if self.tick_counter == self.nb_ticks_per_image:
-            self.tick_counter = 0
-            self.set_next_surface()
+        self.current_line.color_border = COLOR_BORDER_INACTIVE
+        self.current_line.update_surface()
+        self.processor.tick()
+        self.current_line = self.lines[self.processor.stack_pointer]
+        self.current_line.color_border = COLOR_BORDER_EXEC
+        self.current_line.update_surface()
         
+        self.register_gui.set_digits(self.processor.register)
+        
+    def set_program(self, list_lines):
+        for l, text in zip(self.lines, list_lines):
+            l.set_text(text)
+            
+    def get_program(self):
+        return [l.text for l in self.lines]
+    
+    
--- diffengine/main.py
+++ diffengine/main.py
@@ -1,257 +1,137 @@
-"""
-Communication modules for implement various encoding and decoding schemes
+import pygame as pg
+import sys
+from camera import Camera
+from logic import Processor
+from entity import InputBox, ProcessorGUI
+from world import World
 
-Compute energy consumption per bit, 
-    each instruction used requires one unit of energy
-
-cost, 
-    based on performance of memory, 
-    number of lines of instruction
-    register size
-    number of steps
-    energy used
-
-
-Protocols with bidirectional communication e.g 
-    parity encoding with resend option 
-    using redundant encoding 
+def display_scene(camera, world, surface, background):    
+    surface.blit(background, (0,0))
+    rect = camera.get_rect()
+    objects = world.get_objects_ordered_for_display(rect)
+    xc, yc = camera.get_position()
+    for o in objects:
+        o.draw(surface, xc, yc)
+        # xo, yo = o.get_position()
+        # surface.blit(o.get_surface(), (xo-xc, yo-yc))
     
-Connect multiple machines in networks with noisy channels
-have input buffers with finite size which can fill up -> requires larger buffer, 
-    multiple channels, 
-    or better encoders / decoders
-
-Get monetary return based on information exchanged on channels and trust in the channels
-
-Remote software deployment
-Reverse engineering existing designs
-Compression
-Communication protocols
-Encryption
-Hacking
-
-
-"""
-
-
-NO_DATA = 1
-
-def bit_copy(register, source, dest):
-    register[dest] = register[source]
-
-def bit_flip(register, address):
-    register[address] = 0 if register[address] else 1
-
-def bit_xor(register, src1, src2):  
-    """
-    in place bit xor
-    """
-    register[src2] = (register[src1] + register[src2]) % 2
+     
+def main_loop(world, screen, screen_size):
+    main_surface = pg.Surface(screen_size)
+    background = pg.Surface(main_surface.get_size())
+    background = background.convert()
+    # background.fill([60,197,255])
+    background.fill([0,0,0])
+    clock = pg.time.Clock()
     
-def rshift(r):
-    r[:] = [r[-1]] + r[:-1]
-
-def lshift(r):
-    r[:] = r[1:] + [r[0]]
+    camera = Camera(screen.get_rect())
+    # font = pg.font.SysFont("Courier New", 32, bold=True)
+    hover_object = None
     
-def xor(registerA, registerB):
-    """
-    Applies xor between register A and register B.
-    Stores the result in register B
-    """
-    registerB[:] = [(i + j) % 2 for i, j in zip(registerA, registerB)]
+    pg.key.set_repeat(200, 20)
+  
+    i_world_tick_divs = 4
+    world_tick_divs = [1, 2, 5, 10, 30, 60, 120]
     
-def cp_register(reg_source, reg_dest):
-    reg_dest[:] = reg_source[:]
+    world_tick_incr = 0
     
-    
-def _reg_to_int(reg):
-    _reg = reg[:]
-    _reg.reverse()
-    return  (int "0b" + "".join(_ref) )
-    
-def _int_to_reg(a, size=8):
-    s = bin(a)[2:]
-    reg = [0] * size
-    s.reverse()
-    for i, c in enumerate(s):
-        reg[i] = int(c)
-    
-    return reg
-    
-def add(reg_source, reg_dest):
-    """
-    addition between the two registers, stores the result in dest
-    """
-    
-    n = len(reg_source)
-    if n != len(reg_dest):
-        raise ValueError("Register sizes should be equal")
-       
-    a = _reg_to_int(reg_source)
-    b = _reg_to_int(_reg_dest)
-    maxv = 2 ** n
-    
-    c = (a + b) % maxv
-    reg_dest[:] = _int_to_reg(c)
-    
-def push_reg_to_ram(reg, ram, address):
-    ram[address] = _reg_to_int(reg)
-
-def pull_reg_from_ram(reg, ram, address):
-    reg[:] = _int_to_reg(ram[address])
-    
-    
-class CommModule():
-    """
-    Composed of:
-        - a register
-        - a buffer having same size as register
-        - An internal random access memory 
-        - An instruction set
-        - A memory to store the program
-        
-        Information is stored in binary in the registers
-        Each memory slot can store a full integer between 0 and 2**register_size - 1
-        
-    """
-    
-    def __init__(self, register_size=8, memory_size=32, program_size=16):
-        self.register = [0] * register_size   # 0 / 1
-        self.buffer = [0] * register_size   # 0 / 1
-        self.ram = [0] * memory_size # 0 to 255 per slot
-        self.program = [""] * program_size
-        self.register_size = register_size
-        self.stack_pointer = 0 # point to the instruction to execute in the program
-        
-        self.dict_operations = {
-            "READ": self.read,
-            "SEND": self.send,
-            "SWAP": self.swap_reg, 
-            "ADD": lambda add(self.buffer, self.register),
-            "RESET": self.reset_reg,
-            "SAVE": self.save_to_ram,
-            "LOAD": self.load_from_ram,
-            "BITCOPY": lambda src, dest: bit_copy(self.register, src, dest),
-            "BITFLIP": lambda addr: bit_flip(self.register, addr),
-            "BITXOR": lambda src, dest: bit_copy(self.register, src, dest),
-            "BRM": self.branch_msb,
-            "BRL": self.branch_lsb,
-            "RSHIFT": lambda rshift(self.register),
-            "LSHIFT": lambda lshift(self.register),
-        }
-        
-    def branch_msb(self, stack_index):
-        if self.register[-1]:
-            self.stack_pointer = stack_index
+    while 1:
+        dt = clock.tick(30.0) / 30.0
+        for e in pg.event.get():
+            if e.type == pg.QUIT:
+                pg.quit()
+                return
+            if e.type == pg.KEYDOWN:
+                if e.key == pg.K_ESCAPE:
+                    pg.quit()
+                    return
+            if e.type == pg.MOUSEBUTTONDOWN:
+                
+                if e.button == 1:
+                    pos = pg.mouse.get_pos()
+                    
+                    active_object = world.select_at(camera.pix_to_position(pg.mouse.get_pos()))
+                    world.activate(active_object)
+                    
+                elif e.button == 4 : #Wheel-UP
+                    print ("Wheel up")
+                    if i_world_tick_divs>0:
+                        i_world_tick_divs -= 1
+                
+                elif e.button == 5 : #Wheel-down
+                    if i_world_tick_divs< len(world_tick_divs) - 1:
+                        i_world_tick_divs += 1
+                    print ("Wheel down")
+                else:
+                    print ("click %d" % e.button )
             
-    def branch_lsb(self, stack_index):
-        if self.register[0]:
-            self.stack_pointer = stack_index
+            if world.active_object:
+                world.active_object.handle_event(e)
         
-    def swap_reg(self):
-        b = self.buffer 
-        self.buffer[:] = self.register[:]
-        self.register[:] = b
-        
-    def connect_input(self, input_stream):
-        self.input_stream = input_stream
-        
-    def connect_output(self, output_stream):
-        self.output_stream = output_stream
-        
-    def load_from_ram(self, i):
-        self.buffer[:] = _int_to_reg(self.ram[i])
-        
-    def save_to_ram(self, i):
-        self.ram[i] = _reg_to_int(self.buffer)
-        
-    def set_prog_line(i, instruction):
-        self.program[i] = instruction
-        
-    def read(self, size=1):
-        """
-        size from 0 up to input register size
-        1 - read as many bits as size
-        2 - store them in the register (with the first read being the LSB)
-
-        [LSB, ..., MSB] <- the register stores the bits backwards to help with the inner machinery
-        
-        """
-        if len(input_stream) < size:
-            return NO_DATA
+        ## Highlighting the current object
+        if hover_object!= None:
+            hover_object.unhighlight()
             
-        i = 0
-        while i < size:        
-            self.register[i] = input_stream.pop()
-            i += 1
-        
-        return 0
-        
-    def send(self, size=None):
-        """
-        send the full output_register
-        """
-        
-        """
-        send from LSB (least significant) to MSB 
-        """
-        
-        if size is None:
-            size = len(self.register)
-        for i in range(size):
-            self.output_stream.insert(0, self.register[i])
-    
-    def reset_reg(self):
-        self.register[:] = [0] * self.register_size
-    
-    def execute_instruction(self, instruction_line):
-        """
-        One instruction per line
-        When the last line is done, the program loops back to the first line
-        
-        # I/O
-        READ <size in bits> - blocking read
-        SEND <size in bits>
-        
-        # Register manipulation
-        SWAP : swap content of register and buffer
-        ADD: add buffer and register values, store result in register
-        RESET: reset register to 0
-        RSHIFT
-        LSHIFT
-        
-        # For units having RAM
-        SAVE <memory_address>: copy buffer content to memory block
-        LOAD <memory_address>: load memory block to buffer
-        
-        # Bitwise operations
-        in all bit operations, src and dest are an integer having representing the bit weight
-        
-        BITCOPY <src> <dest>
-        BITFLIP <src>
-        BITXOR <src> <dest>        
-        
-        # Branching
-        BRCHMSB <program stack line index> - branch to program stack line if the most significant bit of the register is set to 1
-        BRCHLSB <program stack line index> - branch to program stack line if the least significant bit of the register is set to 1
-        BUF keyword can be used in place of any integer arguments. This takes the value in the buffer
-        
-        """
-        splitted = instruction_line.replace('\n','').split(' ')
-        operator = splitted[0]
-        arguments = [int(x) if x != 'BUF' else _reg_to_int(self.buffer) for x in splitted[1:]]
-        f = self.dict_operations(operator)
-        return f(*arguments)
-         
-    def tick(self):
-        line = self.program[self.stack_pointer]
-        res = self.execute_instruction(line)
-        if res in [None, 0]:
-            self.stack_pointer += 1
+        hover_object = world.select_at(camera.pix_to_position(pg.mouse.get_pos()))
+        if hover_object!= None:
+            hover_object.highlight()
             
-        if self.stack_pointer > len(self.program):
-            self.stack_pointer = 0
+        ## Move the camera
+        key = pg.key.get_pressed()
+        if key[pg.K_LEFT]:
+            camera.move_left(dt)
+        if key[pg.K_RIGHT]:
+            camera.move_right(dt)
+        if key[pg.K_UP]:
+           camera.move_up(dt)
+        if key[pg.K_DOWN]:
+           camera.move_down(dt)
+        if key[pg.K_SPACE]:
+            world.dbg_get_objects(camera.get_rect())
         
         
-
+        if key[pg.K_PLUS]:
+            if i_world_tick_divs>0:
+                i_world_tick_divs -= 1
+        
+        if key[pg.K_MINUS]:
+            if i_world_tick_divs< len(world_tick_divs):
+                i_world_tick_divs += 1
+        
+        #######################
+        #### BEGIN DRAWING ####
+        #######################
+        
+        display_scene(camera, world, main_surface, background)
+        screen.blit(main_surface, (0,0))
+        pg.display.flip()
+        if world_tick_incr >= world_tick_divs[i_world_tick_divs]:        
+            world.tick()
+            world_tick_incr = 0
+            
+        world_tick_incr += 1
+        
+def init_pg():
+    pg.init()
+    screen_sizes = pg.display.list_modes()
+    screen_sizes.sort()
+    screen_sizes.reverse()
+    screen_size = screen_sizes[5]
+    # screen = pg.display.set_mode(screen_size, pg.FULLSCREEN)
+    screen = pg.display.set_mode(screen_size)
+    pg.display.toggle_fullscreen()
+    return screen, screen_size     
+    
+if __name__ == "__main__":
+    screen, screen_size = init_pg() 
+    
+    processor = Processor()
+    world = World()
+    
+    text_boxes = [ProcessorGUI(processor=processor, activate_callback=world.activate)]
+    world.add_objects(text_boxes)
+    main_loop(world, screen, screen_size)
+        
+            
+            
+            
